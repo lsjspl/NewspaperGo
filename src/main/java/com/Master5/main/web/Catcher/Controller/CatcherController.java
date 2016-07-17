fd7e13d215724cd52a94bbeec1928b109e999388
liusanjun <liusanjun@live.com>
2016-07-17 21:37:02 +0800

    11

--- src/main/java/com/Master5/main/web/Catcher/Controller/CatcherController.java
+++ src/main/java/com/Master5/main/web/Catcher/Controller/CatcherController.java
@@ -1,20 +1,5 @@
 package com.Master5.main.web.Catcher.Controller;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
