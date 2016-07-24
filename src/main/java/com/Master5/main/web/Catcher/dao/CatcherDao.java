b6bf9096da7b7ac2a3268aa4c22f6e85a2097bb4
liusanjun <liusanjun@live.com>
2016-07-24 16:19:40 +0800

    1111

--- src/main/java/com/Master5/main/web/Catcher/dao/CatcherDao.java
+++ src/main/java/com/Master5/main/web/Catcher/dao/CatcherDao.java
@@ -8,8 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import com.Master5.main.web.Catcher.entry.Catcher;
 
 public interface CatcherDao  extends JpaRepository<Catcher, Integer> {
-	@Query(value="    SELECT catcher.id,urlsinfo.keyWord,urlsinfo.name,urlsinfo.type,catcher.url,catcher.title,catcher.content,catcher.time,catcher.state "+
-			     "    FROM catcher LEFT JOIN urlsinfo ON catcher.urlId = urlsinfo.id and urlsinfo.state=0                            "+
+	@Query(value="    SELECT catcher.id,urlsInfo.keyWord,urlsInfo.name,urlsInfo.type,catcher.url,catcher.title,catcher.content,catcher.time,catcher.state "+
+			     "    FROM catcher LEFT JOIN urlsInfo ON catcher.urlId = urlsInfo.id and urlsInfo.state=0                            "+
 			     "    WHERE content LIKE CONCAT(\"%\", keyWord, \"%\")                                                               ",
 			     nativeQuery=true
 			)
